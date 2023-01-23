e20cde8e5d331a4f6df274ebf2c511956e6fedda
Julia <67640854+julesss-coder@users.noreply.github.com>
2023-01-23 11:36:30 +0100

    Added final version

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,20 +1,12 @@
 public class DoublyLinkedList<T> {
-    // Should head and tail be private?
-    ListNode<T> head;
-    ListNode<T> tail;
-    private int nodeCounter = 0;
-
-//    @Override
-//    public String toString() {
-//        return "DoublyLinkedList{" +
-//                "head=" + head +
-//                ", tail=" + tail +
-//                '}';
-//    }
+    private ListNode<T> head;
+    private ListNode<T> tail;
+    private int nodeCounter;
 
     public DoublyLinkedList(ListNode<T> head, ListNode<T> tail) {
         this.head = head;
         this.tail = tail;
+        nodeCounter = 0;
     }
 
     // Leave this constructor so you can call class without arguments, too
@@ -22,35 +14,22 @@ public class DoublyLinkedList<T> {
 
     public void append(T value) {
         ListNode<T> node = new ListNode(value);
-
         if (this.head == null) {
             this.head = node;
             this.tail = node;
             nodeCounter++;
         } else if (this.head == this.tail) {
-            this.head.next = node;
+            this.head.setNext(node);
             this.tail = node;
-            this.tail.previous = this.head;
+            this.tail.setPrevious(this.head);
             nodeCounter++;
-//            System.out.println("this.head.next.value: " + this.head.next.value); // exp 2
-//            System.out.println("this.head.value: " + this.head.value); // exp 1
-//            System.out.println("this.tail.value: " + this.tail.value); // exp 2
-//            System.out.println("this.tail.previous.value: " + this.tail.previous.value); // exp 1
         } else {
-            this.tail.next = node;
-//            System.out.println("line 41: " + this.tail.next.value); //exp: 3
-            node.previous = this.tail;
-//            System.out.println("line 43 : " + node.previous.value); // exp.: 2
+            this.tail.setNext(node);
+            node.setPrevious(this.tail);
             this.tail = node;
-//            System.out.println("line 45 : " + this.tail.value); // exp.: 3
-            this.tail.next = null;
+            this.tail.setNext(null);
             nodeCounter++;
-//            System.out.println("this.tail.value: " + this.tail.value); // exp.: 3
-//            System.out.println(this.tail.next == null); // should be true
-//            System.out.println("this.tail.previous.value: " + this.tail.previous.value); // 2
         }
-
-        System.out.println("Number of nodes: " + nodeCounter);
     }
 
     public void add(int index, T value) {
@@ -63,8 +42,6 @@ public class DoublyLinkedList<T> {
         }
 
         // Case: no element at given index:
-            //  Does add() insert elements at non-existing indices other than tailIndex + 1? If so, do I have to create empty nodes until I reach the given index?
-                // No. Just throw an error. Index out of bounds.
         if (index > nodeCounter) {
             throw new IndexOutOfBoundsException("Index " + index + " is out of bounds."); // However, it is possible to pass in index = listLength, which is out of bounds, too. => Need better exception message.
         }
@@ -74,35 +51,116 @@ public class DoublyLinkedList<T> {
             ListNode<T> currentNode = this.head;
 
             if (index == 0) {
-//                currentNode.previous = newNode;
-//                newNode.next = currentNode;
-//                this.head = newNode;
-                this.head.previous = newNode;
-                newNode.next = this.head;
+                this.head.setPrevious(newNode);
+                newNode.setNext(this.head);
                 this.head = newNode;
-                nodeCounter++;
-//                System.out.println("add() runs");
-//                System.out.println("this.head.value " + this.head.value); // exp.: 0
-//                System.out.println("this.head.next.value " + this.head.next.value); // exp.: 1
-//                System.out.println("this.head.next.previous.value " + this.head.next.previous.value); // exp.: 0
             } else {
                 while (indexCounter != index) {
                     indexCounter++;
-                    currentNode = currentNode.next;
+                    currentNode = currentNode.getNext();
                 }
-                ListNode<T> leftNode = currentNode.previous;
-                leftNode.next = newNode;
-                newNode.previous = leftNode;
-                newNode.next = currentNode;
-                currentNode.previous = newNode;
-                nodeCounter++;
+                ListNode<T> leftNode = currentNode.getPrevious();
+                leftNode.setNext(newNode);
+                newNode.setPrevious(leftNode);
+                newNode.setNext(currentNode);
+                currentNode.setPrevious(newNode);
             }
+            nodeCounter++;
         }
-
-        System.out.println("Number of nodes: " + nodeCounter);
     }
 
     public int listLength() {
         return nodeCounter;
+    }
+
+    public void printList() {
+        ListNode<T> node = this.head;
+        if (node == null) {
+            System.out.println("List is empty.");
+        } else {
+            System.out.println("Doubly linked list: ");
+            while (node != null) {
+                System.out.println(node.getValue());
+                node = node.getNext();
+            }
+        }
+    }
+
+    public void printListReverse() {
+        if (this.head == null) {
+            System.out.println("List is empty.");
+        } else {
+            ListNode<T> node = this.tail;
+            while (node != null) {
+                System.out.println(node.getValue());
+                node = node.getPrevious();
+            }
+        }
+    }
+
+    public T searchList(int index) {
+        if (index >=  nodeCounter) {
+            throw new IndexOutOfBoundsException("Index is out of bounds.");
+        }
+
+        int currentIndex = 0;
+        ListNode<T> node = this.head;
+        while (node != null) {
+            if (currentIndex == index) {
+                return node.getValue();
+            }
+            currentIndex++;
+            node = node.getNext();
+        }
+
+        return null;
+    }
+
+    public T searchListReverse(int index) {
+        if (index >= nodeCounter) {
+            throw new IndexOutOfBoundsException("Index is out of bounds.");
+        }
+
+        ListNode<T> node = this.tail;
+        int currentIndex = this.listLength() - 1;
+
+        while (node != null) {
+            if (currentIndex == index) {
+                return node.getValue();
+            }
+            currentIndex--;
+            node = node.getPrevious();
+        }
+
+        return null;
+    }
+
+    public void delete(int index) {
+        if (index >= nodeCounter) {
+            throw new IndexOutOfBoundsException("Index is out of bounds.");
+        }
+
+        ListNode<T> node = this.head;
+        int currentIndex = 0;
+        while (node != null) {
+            if (currentIndex == index) {
+                if (index == 0) {
+                    this.head = node.getNext();
+                    node.getNext().setPrevious(null);
+                } else if (index == nodeCounter - 1) {
+                    this.tail = node.getPrevious();
+                    node.getPrevious().setNext(null);
+                } else {
+                    ListNode<T> leftNode = node.getPrevious();
+                    ListNode<T> rightNode = node.getNext();
+                    leftNode.setNext(rightNode);
+                    rightNode.setPrevious(leftNode);
+                }
+                nodeCounter--;
+                return;
+            }
+            node = node.getNext();
+            currentIndex++;
+        }
     }
 }
